e973b2da60c35acba078510d76f8e580a6b6d858
sodeeq olapade <olapadeabiodun21@email.com>
2019-09-02 16:33:22 +0100

    added a middleware to ensure only admins can work on team route

--- routes/team.route.js
+++ routes/team.route.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const { celebrate: validate } = require('celebrate');
+const httpStatus = require('http-status');
 const teamCtrl = require('../controllers/team.controller');
 const auth = require('../middlewares/auth.middleware');
 const router = express.Router();
 const validation = require('../validations/team.validation');
+const APIError = require('../helpers/APIError');
+/**
+ * Authentication middleware
+ */
+router.use(auth);
+
+
+/**
+ * this middleware ensures only an admin gets 
+ * throught to the team route
+ */
+router.use((req, res, next) => {
+  if (!req.user.isAdmin) {
+    throw new APIError({
+      message: 'Authorization Denied!'
+    });
+  }
+  next();
+});
 
 /**
  * preloads the team with the id supplied and
  * attaches it to req object
  */
 router.param('id', teamCtrl.preLoad);
-
-/**
- * Authentication middleware
- */
-router.use(auth);
 
 /**
  * route to GET all teams
